3cdf7b82e720da36e9c36ea58ac090852499f44a
Rafael Rios <rvrios22@gmail.com>
2025-12-31 16:48:01 -0800

    remove: actions.ts

---
+++ convex/schema.ts
@@ -0,0 +1,9 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export default defineSchema({
+  waitlist: defineTable({
+    name: v.string(),
+    email: v.string(),
+  }).index("by_email", ["email"]),
+});
