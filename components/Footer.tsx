29960d06050bff135cec29eea8d3caa9f6109393
Rafael Rios <rvrios22@gmail.com>
2026-01-09 07:33:48 -0800

    add favicon and remove console log

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,5 @@
-import React from "react";
-
 function Footer() {
   const year = new Date().getFullYear();
-  console.log(year);
   return (
     <footer className="flex h-48 items-end bg-slate-900 text-white">
       <p className="p-4">© {year} SoloPest. All rights reserved.</p>
